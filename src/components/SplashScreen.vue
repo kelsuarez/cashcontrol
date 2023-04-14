f294f24943866ca1369d42502190cd81167e0619
KELVIN <kelsuarez22@gmail.com>
2023-04-14 18:24:35 +0200

    SplashScreen

---
+++ src/components/SplashScreen.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="splashscreen">
+        <img 
+            src="@/assets/cashflow-icon_070076db-e83a-4afa-b18d-ee058c2b646e.svg" 
+            alt="logo"
+        />
+        <h1><span>Cash</span>Control</h1>
+    </div>
+</template>
+
+<style scoped>
+.splashscreen {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
+}
+h1 {
+  color: var(--brand-blue);
+}
+h1 span {
+  color: var(--brand-green);
+}
+</style>
