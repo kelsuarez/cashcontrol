32040e3a75619458f147152ada7f40897772825d
KELVIN <kelsuarez22@gmail.com>
2023-04-14 23:29:53 +0200

    historique

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -19,7 +19,9 @@
             </Resume>
         </template>
         <template #movements>
-            <Movements/>
+            <Movements
+                :movements="movements"
+            />
         </template>
     </Layout>
 </template>
@@ -28,7 +30,7 @@
 import Layout from './Layout.vue';
 import Header from './Header.vue';
 import Resume from './Resume/Index.vue';
-import Movements from './Movements.vue';
+import Movements from './Movements/Index.vue';
 
 export default {
     components: {
@@ -41,6 +43,52 @@ export default {
         return {
             label: null,
             amount: null,
+            movements: [{
+                id: 0,
+                title: "Mouvement 1",
+                description: "Lorem ipsul dolor sit amet",
+                amount: 1000,
+            }, {
+                id: 1,
+                title: "Mouvement 2",
+                description: "Lorem ipsul dolor sit amet",
+                amount: 1000,
+            }, {
+                id: 2,
+                title: "Mouvement 3",
+                description: "Lorem ipsul dolor sit amet",
+                amount: 1000,
+            }, {
+                id: 3,
+                title: "Mouvement 4",
+                description: "Lorem ipsul dolor sit amet",
+                amount: 1000,
+            }, {
+                id: 4,
+                title: "Mouvement 5",
+                description: "Lorem ipsul dolor sit amet",
+                amount: 1000,
+            }, {
+                id: 5,
+                title: "Mouvement 6",
+                description: "Lorem ipsul dolor sit amet",
+                amount: 1000,
+            }, {
+                id: 6,
+                title: "Mouvement 7",
+                description: "Lorem ipsul dolor sit amet",
+                amount: 1000,
+            }, {
+                id: 7,
+                title: "Mouvement 8",
+                description: "Lorem ipsul dolor sit amet",
+                amount: 1000,
+            }, {
+                id: 8,
+                title: "Mouvement 9",
+                description: "Lorem ipsul dolor sit amet",
+                amount: 1000,
+            }]
         }
     }
 };
