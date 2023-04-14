c261c33185ffa4e1d3338b0e77a3410d71a70ee1
KELVIN <kelsuarez22@gmail.com>
2023-04-14 22:28:20 +0200

    Layout

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,29 @@
 <template>
-    <div>Hola mundo</div>
+    <Layout>
+        <template #header>
+            <Header></Header>
+        </template>
+        <template #resume>
+            <Resume/>
+        </template>
+        <template #movements>
+            <Movements/>
+        </template>
+    </Layout>
 </template>
+
+<script>
+import Layout from './Layout.vue';
+import Header from './Header.vue';
+import Resume from './Resume/Index.vue';
+import Movements from './Movements.vue';
+
+export default {
+    components: {
+        Layout,
+        Header,
+        Resume,
+        Movements,
+    }
+};
+</script>
