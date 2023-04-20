abb4116d7a2b550753957cffa9823cec471bac98
KELVIN <kelsuarez22@gmail.com>
2023-04-20 19:31:55 +0200

    interaccion grafico

--- src/components/Resume/Graphic.vue
+++ src/components/Resume/Graphic.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
-        <svg 
+        <svg
+            @touchstart="tap"
+            @touchmove="tap"
+            @touchend="untap"
             viewBox="0 0 300 200"
         >
             <line
@@ -11,71 +14,72 @@
                 x2="300"
                 :y2="zero"
             />
-            <line
-                stroke="#04b500"
-                stroke-width="2"
-                x1="200"
-                y1="0"
-                x2="200"
-                y2="200"
-            />
             <polyline
                 fill="none"
                 stroke="#0689B0"
                 stroke-width="2"
                 :points="points"
             />
+            <line
+                v-show="showPointer"
+                stroke="#04b500"
+                stroke-width="2"
+                :x1="pointer"
+                y1="0"
+                :x2="pointer"
+                y2="200"
+            />
         </svg>
         <p>Dernières 30 jours</p>
-        <div>{{ zero }}</div>
     </div>
 </template>
 
 <script setup>
-
-import { defineProps, toRefs, computed } from 'vue';
-
+import { ref, toRefs, defineProps, computed } from 'vue';
 const props = defineProps({
     amounts: {
         type: Array,
         default: () => [],
     }
 });
-
 const { amounts } = toRefs(props);
-
 const amountToPixels = (amount) => {
     const min = Math.min(...amounts.value);
     const max = Math.max(...amounts.value);
-
     const amountAbs = amount + Math.abs(min);
     const minmax = Math.abs(max) + Math.abs(min);
-
     return 200 - ((amountAbs * 100) / minmax) * 2;
 }
-
-const zero = computed (() => {
+const zero = computed(() => {
     return amountToPixels(0);
 });
-
-const points = computed (() => {
+const points = computed(() => {
     const total = amounts.value.length;
-     
     return amounts.value.reduce((points, amount, i) => {
         const x = (300 / total) * (i + 1);
         const y = amountToPixels(amount);
-        console.log(y)
-        return `${points} ${x}, ${y}`;
-    }, "0,100");
+        return `${points} ${x},${y}`;
+    }, "0, 100");
 });
-
+const showPointer = ref(false);
+const pointer = ref(0);
+const tap = ({ target, touches }) => {
+    showPointer.value = true;
+    const elementWidth = target.getBoundingClientRect().width;
+    const elementX = target.getBoundingClientRect().x;
+    const touchX = touches[0].clientX;
+    pointer.value = ((touchX - elementX) * 300) / elementWidth;
+}
+const untap = () => {
+    showPointer.value = false;
+}
 </script>
 
 <style scoped>
 svg {
-    width: 100%;
+  width: 100%;
 }
 p {
-    text-align: center;
+  text-align: center;
 }
 </style>
